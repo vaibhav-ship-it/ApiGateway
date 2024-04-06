3ec910cb5bb9971ac3a961c108830fd368203c26
Vaibhav <vaibhav0320@gmail.com>
2024-04-06 20:20:36 +0530

    Added new pre and post filters

---
+++ src/main/java/com/appsdeveloperblog/photoapp/api/gateway/GlobalFiltersConfiguration.java
@@ -0,0 +1,56 @@
+package com.appsdeveloperblog.photoapp.api.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import reactor.core.publisher.Mono;
+
+
+/**
+ * 
+ */
+@Configuration
+public class GlobalFiltersConfiguration {
+
+	final Logger logger = LoggerFactory.getLogger(GlobalFiltersConfiguration.class);
+	
+	@Bean
+	@Order(1)
+	public GlobalFilter secondPreFilter()	{
+		
+		return (exchange, chain) -> {
+			logger.info("My second global pre-filter is executed...");
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				logger.info("My third global post-filter was executed...");
+			}));
+		};
+	}
+	
+	@Bean
+	@Order(2)
+	public GlobalFilter thirdPreFilter()	{
+		
+		return (exchange, chain) -> {
+			logger.info("My third global pre-filter is executed...");
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				logger.info("My second global post-filter was executed...");
+			}));
+		};
+	}
+	
+	@Bean
+	@Order(3)
+	public GlobalFilter fourthPreFilter()	{
+		
+		return (exchange, chain) -> {
+			logger.info("My fourth global pre-filter is executed...");
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				logger.info("My first global post-filter was executed...");
+			}));
+		};
+	}
+}
